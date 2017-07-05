ab94d084564027be3e078a172b31b4e7773aaec1
Nedjim <nedjim.danimon@gmail.com>
2017-07-05 14:59:10 +0200

    Init project

---
+++ dev/js/index.js
@@ -0,0 +1,6 @@
+import 'babel-polyfill';
+import React                from 'react';
+import ReactDOM             from 'react-dom';
+
+
+ReactDOM.render( <h1>Hey hey</h1>, document.getElementById('root'));
